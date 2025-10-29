467b0ff1d4b12a661d3acaa44bfd8ebb7b263ac0
YoungAstronaut <2801266493@qq.com>
2025-10-29 07:10:47 +0000

    添加一个调试用的工具函数

--- recipe/mixed_train/tensor_utils.py
+++ recipe/mixed_train/tensor_utils.py
@@ -1,14 +1,28 @@
 import torch
+from tensordict import TensorDict
 
-def print_tensor(tensor: torch.Tensor):
-    print(tensor.shape)
-    print(tensor.dtype)
-    print(tensor.device)
+
+def print_tensor(tensor: torch.Tensor, extra_str: str = None):
+    # print(tensor.shape)
+    # print(tensor.dtype)
+    # print(tensor.device)
     if tensor.dim() >= 2:
         tensor = tensor[0]
     nums = tensor.tolist()
     nums = [str( num) for num in nums]
-    str_to_print = ' | '.join(nums)
+    str_to_print = '\n' +'\t'.join(nums)
     print('--------------')
+    if extra_str:
+        print(extra_str)
     print(str_to_print)
     print('--------------')
+
+def print_tensor_dict(tensor_dict: TensorDict, extra_str: str = None, keys: list = None):
+    if extra_str:
+        print(extra_str)
+    if keys:
+        for key in keys:
+            print_tensor(tensor_dict[key], key)
+        return
+    for key, value in tensor_dict.items():
+        print_tensor(value, key)
