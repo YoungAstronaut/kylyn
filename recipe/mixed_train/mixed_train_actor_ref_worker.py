c24c424634742bffa8f20a76fe609fc73acc79da
YoungAstronaut <2801266493@qq.com>
2025-12-11 07:08:16 +0000

    更改了compute_log_prob函数，额外返回一组eos_prob

--- recipe/mixed_train/mixed_train_actor_ref_worker.py
+++ recipe/mixed_train/mixed_train_actor_ref_worker.py
@@ -26,7 +26,7 @@ from verl.utils.fs import copy_to_local
 from verl.utils.fsdp_utils import (
     fsdp_version,
     offload_fsdp_model_to_cpu,
-    offload_fsdp_optimizer,
+    offload_fsdp_optimizer, load_fsdp_model_to_gpu,
 )
 from verl.utils.import_utils import import_external_libs
 from verl.utils.profiler import log_gpu_memory_usage, DistProfiler, simple_timer
@@ -116,6 +116,53 @@ class MixedTrainActorRefWorker(ActorRolloutRefWorker):
 
         return rollout_worker, rollout_sharding_manager
 
+    @register(dispatch_mode=make_nd_compute_dataproto_dispatch_fn(mesh_name="actor"))
+    @DistProfiler.annotate(color="blue", role="actor_compute_log_prob")
+    def compute_log_prob(self, data: DataProto):
+        # when is_lora is True, we use the actor without lora applied to calculate the log_prob
+        # which is mostly used for ref log_prob calculation
+        assert self._is_actor
+        if self._is_offload_param:
+            load_fsdp_model_to_gpu(self.actor_module_fsdp)
+
+        # Support all hardwares
+        from contextlib import nullcontext
+
+        is_lora = data.meta_info.pop("is_lora", False)
+        adapter_ctx = self.actor.actor_module.disable_adapter() if is_lora else nullcontext()
+
+        # 获取eos_token_id
+        eos_token_id = data.meta_info.pop("eos_token_id", -1)
+        assert eos_token_id >= 0, 'please specify eos_token_id'
+
+        # we should always recompute old_log_probs when it is HybridEngine
+        data.meta_info["micro_batch_size"] = self.config.rollout.log_prob_micro_batch_size_per_gpu
+        data.meta_info["max_token_len"] = self.config.rollout.log_prob_max_token_len_per_gpu
+        data.meta_info["use_dynamic_bsz"] = self.config.rollout.log_prob_use_dynamic_bsz
+        data.meta_info["temperature"] = self.config.rollout.temperature
+        # perform recompute log_prob
+        with self.ulysses_sharding_manager:
+            with adapter_ctx:
+                output, entropys, eos_prob = self.actor.compute_log_prob(
+                    data=data, calculate_entropy=True, need_eos_prob=True, eos_token_id=eos_token_id) # 更改了调用参数
+            output = DataProto.from_dict(
+                tensors={"old_log_probs": output, "entropys": entropys, "eos_prob": eos_prob},
+                meta_info={"temperature": self.config.rollout.temperature},
+            )
+
+        output = output.to("cpu")
+
+        # https://pytorch.org/docs/stable/notes/fsdp.html#fsdp-notes
+        # unshard the root FSDP module
+        if self.world_size > 1 and fsdp_version(self.actor.actor_module) == 1:
+            self.actor.actor_module._handle.reshard(True)
+
+        if self._is_offload_param:
+            offload_fsdp_model_to_cpu(self.actor_module_fsdp)
+            log_gpu_memory_usage("After offload actor model during compute_log_prob", logger=logger)
+
+        return output
+
     @register(dispatch_mode=Dispatch.ONE_TO_ALL)
     def init_model(self):
         # This is used to import external_lib into the huggingface systems
@@ -128,6 +175,7 @@ class MixedTrainActorRefWorker(ActorRolloutRefWorker):
         use_remove_padding = self.config.model.get("use_remove_padding", False)
         use_shm = self.config.model.get("use_shm", False)
         use_fused_kernels = self.config.model.get("use_fused_kernels", False)
+        print(f'#### is actor: {self._is_actor} is rollout: {self._is_rollout} is ref: {self._is_ref}')
 
         if self._is_actor or self._is_rollout:
             # we need the model for actor and rollout
@@ -172,6 +220,7 @@ class MixedTrainActorRefWorker(ActorRolloutRefWorker):
             self.rollout, self.rollout_sharding_manager = self._build_rollout(
                 trust_remote_code=self.config.model.get("trust_remote_code", False)
             )
+            # self.rollout.rollout.inference_engine.sleep()
 
         if self._is_ref:
             self.ref_module_fsdp = self._build_model_optimizer(
@@ -210,8 +259,9 @@ class MixedTrainActorRefWorker(ActorRolloutRefWorker):
 
     @register(dispatch_mode=make_nd_compute_dataproto_dispatch_fn(mesh_name="rollout"))
     @DistProfiler.annotate(color="red", role="rollout_generate")
-    def generate_sft_blocks(self, prompts: DataProto):
+    def generate_se_blocks(self, prompts: DataProto):
         # Support all hardwares
+        print("generate_sft_blocks actor worker PID", os.getpid(), "VLLM_USE_V1=", os.environ.get("VLLM_USE_V1"))
         prompts = prompts.to(get_device_id())
 
         assert self._is_rollout
